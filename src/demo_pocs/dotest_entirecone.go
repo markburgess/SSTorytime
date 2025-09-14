eb0deccb3a6a109e4c3c471167261905d01690e4
mark <mark.burgess.oslo.mb@gmail.com>
2025-09-14 10:26:02 +0200

    update test for consistent cones, recognizing retrieval order is random

--- src/demo_pocs/dotest_entirecone.go
+++ src/demo_pocs/dotest_entirecone.go
@@ -54,20 +54,30 @@ func Solve(ctx SST.PoSST) {
 
 	cntx := []string{""}
 
-	const limit = 10
+	// Note, because retrieval is non-deterministic in order, if we make this small
+	// the probability of getting the same set decreases. So grab everything.
+
+	const limit = 30
 
 	for turn := 0; ldepth < maxdepth && rdepth < maxdepth; turn++ {
 
 		left_paths,Lnum = SST.GetEntireNCConePathsAsLinks(ctx,"fwd",leftptrs[0],ldepth,"",cntx,limit)
 		xleft_paths,Lnumx := SST.GetEntireConePathsAsLinks(ctx,"fwd",leftptrs[0],ldepth,limit)
+
 		right_paths,Rnum = SST.GetEntireNCConePathsAsLinks(ctx,"bwd",rightptrs[0],rdepth,"",cntx,limit)
 		xright_paths,Rnumx := SST.GetEntireConePathsAsLinks(ctx,"bwd",rightptrs[0],rdepth,limit)	
+
 		if Lnum != Lnumx {
 			fmt.Println("LEFT sizes differ at depth",ldepth,"=",Lnum,Lnumx)
+			fmt.Println("EntireNCcone",left_paths)
+			fmt.Println("Entirecone",xleft_paths)
 			os.Exit(-1)
 		}
+
 		if Diff(left_paths,xleft_paths) {
 			fmt.Println("LEFT SETS differ at depth",ldepth)
+			fmt.Println("EntireNCcone",left_paths)
+			fmt.Println("Entirecone",xleft_paths)
 			os.Exit(-1)
 		}
 
@@ -75,6 +85,7 @@ func Solve(ctx SST.PoSST) {
 			fmt.Println("RIGHT sizes differ at depth",rdepth,"=",Rnum,Rnumx)
 			os.Exit(-1)
 		}
+
 		if Diff(right_paths,xright_paths) {
 			fmt.Println("RIGHT SETS differ at depth",rdepth)
 			os.Exit(-1)
@@ -89,26 +100,38 @@ func Solve(ctx SST.PoSST) {
 
 func Diff(left,right [][]SST.Link) bool {
 
-	var LRsplice = make(map[int]int)
-	var list string
+	var L = make(map[SST.Link]bool)
+	var R = make(map[SST.Link]bool)
 
-	// Return coordinate pairs of partial paths to splice
+	retval := false
 
 	for path := 0; path < len(left); path++ {
 		for l := 0; l < len(left[path]); l++ {
-			if left[path][l].Dst != right[path][l].Dst {
-				fmt.Println("Mismatch at path",path,l)
-				fmt.Println("Mismatch:",left[path][l],right[path][l])
-				return true
-			}
+			L[left[path][l]] = true
 		}
 	}
 
-	if len(list) > 0 {
-		fmt.Println("  (i.e. waves impinge",len(LRsplice),"times at: ",list,")\n")
+	for path := 0; path < len(left); path++ {
+		for l := 0; l < len(left[path]); l++ {
+			R[right[path][l]] = true
+		}
 	}
 
-	return false
+	for r := range L {
+		if !R[r] {
+			fmt.Println("L not in R",r)
+			retval = true
+		}
+	}
+
+	for l := range R {
+		if !L[l] {
+			fmt.Println("R not in L",l)
+			retval = true
+		}
+	}
+
+	return retval
 }
 
 

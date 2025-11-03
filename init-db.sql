7a2cd1e3e7560b3d7ab9ecbb8116ac280e38dbb7
mark <mark.burgess.oslo.mb@gmail.com>
2025-11-03 20:56:16 +0100

    Remove all docker files merges, due to incomplete PR

--- init-db.sql
+++
@@ -1,8 +0,0 @@
--- Initialize SSTorytime database
--- This runs automatically when postgres container starts
-
--- Create unaccent extension for text search
-CREATE EXTENSION IF NOT EXISTS unaccent;
-
--- Grant all privileges
-GRANT ALL PRIVILEGES ON DATABASE sstoryline TO sstoryline;

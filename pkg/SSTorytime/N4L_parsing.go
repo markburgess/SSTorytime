e41e85bf7e0c4b8571411cd9bc6e5f6963c9fec0
mark <mark.burgess.oslo.mb@gmail.com>
2026-09-16 10:00:05 +0200

    formatting

--- pkg/SSTorytime/N4L_parsing.go
+++ pkg/SSTorytime/N4L_parsing.go
@@ -112,7 +112,7 @@ func CheckExisting(sst *PoSST,event Node) (ClassedNodePtr,bool) {
 
 		if db_exists != nil {
 			
-			fmt.Println("Matches for",event.S,"already exist in the database. You need to reload all overlapping segments together to avoid double registration.")
+			fmt.Println("Matches for '",event.S,"' already exist in the database. You need to reload all overlapping segments together to avoid double registration.")
 		}
 	}
 	
